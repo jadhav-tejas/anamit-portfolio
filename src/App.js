0d7a005aa028f3dbd6594671f186f9616dd9fbca
TejasNitinJadhav <jadhavtejas0072@gmail.com>
2024-02-24 16:43:12 +0530

    Added navbar, CSS, and assets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
-import NavBar from "./components/NavBar";
+import { NavBar } from "./components/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <NavBar></NavBar>
+      <NavBar />
     </div>
   );
 }
