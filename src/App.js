136671f3815d05ec8b968741fe42c67c23ca49c3
TejasNitinJadhav <jadhavtejas0072@gmail.com>
2024-03-20 21:37:07 +0530

    AMT-02: Added Upcoming section with test image

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Button } from "bootstrap";
 import "./App.css";
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
+import { Upcoming } from "./components/Upcoming";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
+      <Upcoming />
     </div>
   );
 }
